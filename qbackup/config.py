987e3bf939c7f69a9217e31ae575f4d6ff17c278
Alan Chan <chanfung032@gmail.com>
2012-04-02 00:42:07 +0800

    Add qbackup

---
+++ qbackup/config.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+BACKUP_EMAIL = '<email adress to backup>'
+
+SMTP_HOST = 'smtp.qq.com'
+SMTP_USR  = 'username@qq.com'
+SMTP_PWD  = '1234567890'
+
+MAIL_FROM = 'XXX <1234567890@qq.com>'
+MAIL_TO   = 'OOO <whowhowho@gmail.com>'
+
+QQ_USER_DICT = {
+    # qq               nickname           real-name 
+    '12345678901':     ('nickname',      'realname'      ),
+}
+
